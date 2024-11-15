0a2e1a35fb2979746c01aeb916835805f534e238
andrey <uvarov.advokat@gmail.com>
2024-11-15 15:33:28 +0200

    small changes

--- src/main/groovy/com/chat/model/User.groovy
+++ src/main/groovy/com/chat/model/User.groovy
@@ -2,12 +2,11 @@ package com.chat.model
 
 import com.fasterxml.jackson.annotation.JsonIgnore
 import lombok.Data
-import lombok.NoArgsConstructor
 import org.springframework.data.annotation.Id
 import org.springframework.data.mongodb.core.index.Indexed
 import org.springframework.data.mongodb.core.mapping.Document
-import org.springframework.security.core.GrantedAuthority
-import org.springframework.security.core.authority.SimpleGrantedAuthority
+//import org.springframework.security.core.GrantedAuthority
+//import org.springframework.security.core.authority.SimpleGrantedAuthority
 
 import java.time.Instant
 
@@ -28,9 +27,9 @@ class User {
 
     Instant registeredAt = Instant.now()
 
-    Collection<? extends GrantedAuthority> getAuthorities() {
-        return Collections.singletonList(new SimpleGrantedAuthority("ROLE_" + role));
-    }
+//    Collection<? extends GrantedAuthority> getAuthorities() {
+//        return Collections.singletonList(new SimpleGrantedAuthority("ROLE_" + role));
+//    }
 }
 
 
